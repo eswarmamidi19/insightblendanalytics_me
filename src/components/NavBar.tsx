e61ce95ab8fc39753be2b07a14b091d0516a3962
eswarmamidi19 <eswarmamidi19@gmail.com>
2024-08-19 12:43:32 +0530

    Navbar updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,67 @@
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { Link , useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LuArrowUpRight } from "react-icons/lu";
+import { cn } from "@/utils";
+
+type NavElement = {
+   name : string;
+   location : string;
+   isActive : boolean
+}
+
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [pages,setPages] = useState<NavElement[]>([ {
+   name : "home" , 
+   location : "/" ,
+   isActive : false
+ } , {
+   name : "about" , 
+   location : "/about" ,
+   isActive : false
+ },{
+   name : "courses" , 
+   location : "/course" ,
+   isActive : false
+ },{
+   name : "Team" , 
+   location : "/" ,
+   isActive : false
+ },{
+   name : "Contact Us" , 
+   location : "/" ,
+   isActive : false
+ }])
+
   const handleOpen = () => {
     setOpen((prev) => !prev);
   };
   const navigate = useNavigate();
+
+  const handleMobileNavClick = (page:NavElement) => {
+    const location :string = page.location;
+    setPages((prev)=>{
+       
+       return prev.map((ele) =>{
+        ele.isActive = false;
+         if(ele.name===page.name){
+          ele.isActive = true;
+          return ele;
+        }
+        return ele;
+
+       }) 
+    })
+    handleOpen(); 
+    navigate(location); 
+  };
+
   return (
     <header className="bg-[#0C3531] text-white fixed top-0 z-10 w-full  shadow-nav">
       <section className="sm:max-w-4xl md:max-w-5xl xl:max-w-[1200px] mx-auto p-4 px-10  md:px-4 flex justify-between  items-center ">
-        <img src="/logo.svg" className="w-[30%] md:w-[15%]" onClick={()=>{navigate("/")}} />
+        <img src="/logo.svg" className="w-[30%] md:w-[15%]" onClick={() => { navigate("/") }} />
         <div>
           <button className="text-3xl md:hidden focus:outline-none text-[#ECB148]">
             {open ? (
@@ -30,7 +79,7 @@ export default function Navbar() {
             )}
           </button>
           <nav className="hidden md:block space-x-8 text-sm" aria-label="main">
-          <Link to={"/"} className="hover:opacity-90 font-primaryRegular">
+            <Link to={"/"} className="hover:opacity-90 font-primaryRegular">
               Home
             </Link>
             <Link to={"/about"} className="hover:opacity-90 font-primaryRegular">
@@ -58,37 +107,21 @@ export default function Navbar() {
             className="flex min-h-screen flex-col items-center py-8 "
             aria-label="mobile"
           > 
-          <div className="w-4/5 flex justify-between items-center border-b-[3px] border-[#ECB148] py-7 ">
-            <Link to={"/"}  className="hover:opacity-90 font-primaryRegular text-[#0A574D] text-2xl" >
-              Home
-            </Link>
-            <LuArrowUpRight  className="text-3xl text-[#0A574D]"/>
-          </div>
-           
-          <div className="w-4/5 flex justify-between items-center border-b-[3px] border-[#0A574D] py-7 mt-2">
-            <Link to={"/about"} className="hover:opacity-90 font-primaryRegular text-[#0A574D] text-2xl">
-              About us
-            </Link>
-            <LuArrowUpRight  className="text-3xl text-[#0A574D]"/>
-          </div>
-          <div className="w-4/5 flex justify-between items-center border-b-[3px] border-[#0A574D] py-7 mt-2">
-            <Link to={"/course"} className="hover:opacity-90 font-primaryRegular text-[#0A574D] text-2xl">
-              Courses
-            </Link>
-            <LuArrowUpRight  className="text-3xl text-[#0A574D]"/>
-          </div>
-          <div className="w-4/5 flex justify-between items-center border-b-[3px] border-[#0A574D] py-7 mt-2">
-            <Link to={"/"} className="hover:opacity-90 font-primaryRegular text-[#0A574D] text-2xl">
-              Team
-            </Link>
-            <LuArrowUpRight  className="text-3xl text-[#0A574D]"/>
-          </div>
-          <div className="w-4/5 flex justify-between items-center border-b-[3px] border-[#0A574D] py-7 mt-2">
-            <Link to={"/"} className="hover:opacity-90 font-primaryRegular text-[#0A574D] text-2xl">
-              Contact us
-            </Link>
-            <LuArrowUpRight  className="text-3xl text-[#0A574D]"/>
-          </div>
+             {
+               pages.map((page)=>{
+                return (
+            <div className={cn("w-4/5 flex justify-between items-center border-b-[3px]  py-7 border-[#0A574D]", page.isActive&& "border-[#ECB148]")} key={page.name}>
+            <button
+              onClick={() => handleMobileNavClick(page)}
+              className="hover:opacity-90 font-primaryRegular text-[#0A574D] text-2xl"
+            >
+              {page.name}
+            </button>
+            <LuArrowUpRight className="text-3xl text-[#0A574D]" />
+           </div>
+          )
+           })
+             }
           </nav>
         </section>
       )}
